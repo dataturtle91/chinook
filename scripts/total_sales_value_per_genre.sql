363748fe069e9b32763fd500a97749777bb029de
dataturtle <150851332+dataturtle91@users.noreply.github.com>
2024-04-27 09:55:55 +0200

    Update total_sales_value_per_genre.sql

--- scripts/total_sales_value_per_genre.sql
+++ scripts/total_sales_value_per_genre.sql
@@ -1,22 +1,27 @@
---total sales value per genre
-
 WITH tspg AS (
-    SELECT ii.InvoiceId AS invoice_id,
-           i.InvoiceDate AS date,
-           ii.TrackId AS track_id,
-           t.GenreId AS genre_id,
-           ROUND(SUM(ii.Quantity * ii.UnitPrice) ) AS total_sales
-      FROM invoice_items AS ii
-           LEFT JOIN
-           tracks AS t ON t.TrackId = ii.TrackId
-           LEFT JOIN
-           invoices AS i ON ii.InvoiceId = i.InvoiceId
-     GROUP BY genre_id
-)
-SELECT genres.Name AS genre,
-       tspg.total_sales
-  FROM tspg
+    SELECT 
+           ii.InvoiceId AS invoice_id
+           ,i.InvoiceDate AS date
+           ,ii.TrackId AS track_id
+           ,t.GenreId AS genre_id
+           ,ROUND(SUM(ii.Quantity * ii.UnitPrice) ) AS total_sales
+      FROM 
+          invoice_items AS ii
+           
        LEFT JOIN
-       genres ON tspg.genre_id = genres.GenreId
- ORDER BY total_sales DESC;
+           tracks AS t ON t.TrackId = ii.TrackId
+       LEFT JOIN
+           invoices AS i ON ii.InvoiceId = i.InvoiceId
+       GROUP BY genre_id
+)
 
+SELECT 
+        genres.Name AS genre
+       ,tspg.total_sales
+       ,dense_rank() over (order by total_sales DESC) AS sales_ranking
+FROM 
+      tspg
+LEFT JOIN
+       genres ON tspg.genre_id = genres.GenreId
+ ORDER BY 
+     total_sales DESC
