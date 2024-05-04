0968ee89b70f8a20ff607163b2d8b36f5267500f
dataturtle <150851332+dataturtle91@users.noreply.github.com>
2024-05-04 23:05:03 +0200

    Create total_order_value_per_country.sql

---
+++ scripts/total_order_value_per_country.sql
@@ -0,0 +1,13 @@
+-- Total order value, average order value and number of orders per country
+
+SELECT
+       BillingCountry AS country
+       ,COUNT(CustomerId) AS number_of_purchases
+       ,ROUND(SUM(Total),2) AS total_order_value
+       ,ROUND(ROUND(SUM(Total),2)/COUNT(CustomerId),2) AS average_order_value
+  FROM 
+      invoices  
+GROUP BY 
+    country
+ORDER BY
+    total DESC
